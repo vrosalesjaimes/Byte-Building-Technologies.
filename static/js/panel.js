c2387dbfc9d9615898f69d707a0a5a3dacbee92f
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-06-02 18:56:57 -0600

    Creacion correcta de targetas

--- static/js/panel.js
+++ static/js/panel.js
@@ -9,12 +9,18 @@ $(document).ready(function () {
     const $bebidas_block = $('.bebidas');
     const $postres_block = $('.postres');
     const $categories = $('.content:first');
+    const $menu = $('#return_menu');
+    const $buttons = $('.buttons')
+    const $buttons_menu = $('.buttons_menu')
+    const $header = $('header');
 
     $entrada.click(function () {
         $body.removeClass('body_initial');
         $categories.css('display', 'none');
         $entrada_block.css('margin-top','0%')
         $entrada_block.css('display', 'flex');
+        $buttons.css('display', 'none');
+        $buttons_menu.css('display', 'grid');
     })
 
     $platillo_fuerte.click(function () {
@@ -22,6 +28,8 @@ $(document).ready(function () {
         $categories.css('display', 'none');
         $platos_block.css('margin-top','0%')
         $platos_block.css('display', 'flex');
+        $buttons.css('display', 'none');
+        $buttons_menu.css('display', 'grid');
     })
 
     $bebida.click(function () {
@@ -29,6 +37,8 @@ $(document).ready(function () {
         $categories.css('display', 'none');
         $bebidas_block.css('margin-top','0%')
         $bebidas_block.css('display', 'flex');
+        $buttons.css('display', 'none');
+        $buttons_menu.css('display', 'grid');
     })
 
     $postre.click(function () {
@@ -36,5 +46,18 @@ $(document).ready(function () {
         $categories.css('display', 'none');
         $postres_block.css('margin-top','0%')
         $postres_block.css('display', 'flex');
+        $buttons.css('display', 'none');
+        $buttons_menu.css('display', 'grid');
+    })
+
+    $menu.click(function () {
+        $postres_block.css('display', 'none');
+        $entrada_block.css('display', 'none');
+        $platos_block.css('display', 'none');
+        $bebidas_block.css('display', 'none');
+        $body.addClass('body_initial');
+        $categories.css('display', 'flex');
+        $buttons_menu.css('display', 'none');
+        $buttons.css('display', 'grid');
     })
 })
